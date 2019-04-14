e0e04fcba7fce8f86eed15db5dd5e5a5949401c2
ch4rli3kop <pch2180@gmail.com>
2019-04-14 18:23:11 +0900

    finish __libc_malloc

--- malloc/malloc_glibc-2.25.c
+++ malloc/malloc_glibc-2.25.c
@@ -2882,17 +2882,19 @@ __libc_malloc (size_t bytes)
   mstate ar_ptr;
   void *victim;
 
-  void *(*hook) (size_t, const void *)
-    = atomic_forced_read (__malloc_hook);
+  void *(*hook) (size_t, const void *) 
+    = atomic_forced_read (__malloc_hook); // __malloc_hook에 등록된 주소를 저장한다.
   if (__builtin_expect (hook != NULL, 0))
-    return (*hook)(bytes, RETURN_ADDRESS (0));
+    return (*hook)(bytes, RETURN_ADDRESS (0)); // hook 함수가 등록되어 있다면 hook 함수가 실행된다.
 
-  arena_get (ar_ptr, bytes);
+  arena_get (ar_ptr, bytes); // arena, 즉 mstate의 포인터를 저장한다.
 
-  victim = _int_malloc (ar_ptr, bytes);
+  victim = _int_malloc (ar_ptr, bytes); // arena 포인터와 size를 인자로 _int_malloc()을 호출하여 메모리를 할당받는다. _int_malloc()이 malloc 동작의 핵심임
+  
   /* Retry with another arena only if we were able to find a usable arena
-     before.  */
-  if (!victim && ar_ptr != NULL)
+     before.  
+     사용가능한 다른 arena를 이용하여(찾을 수 있다면) 재할당을 시도한다.*/
+  if (!victim && ar_ptr != NULL) // 첫 시도에서 못 받았다면
     {
       LIBC_PROBE (memory_malloc_retry, 1, bytes);
       ar_ptr = arena_get_retry (ar_ptr, bytes);
@@ -2900,11 +2902,11 @@ __libc_malloc (size_t bytes)
     }
 
   if (ar_ptr != NULL)
-    __libc_lock_unlock (ar_ptr->mutex);
+    __libc_lock_unlock (ar_ptr->mutex); // 할당이 완료되었으므로 lock(mutex)을 해제한다.
 
   assert (!victim || chunk_is_mmapped (mem2chunk (victim)) ||
-          ar_ptr == arena_for_chunk (mem2chunk (victim)));
-  return victim;
+          ar_ptr == arena_for_chunk (mem2chunk (victim))); // 정상적으로 할당이 되었는지 확인한다.
+  return victim; // chunk를 반환한다. (malloc 종료)
 }
 libc_hidden_def (__libc_malloc)
 
